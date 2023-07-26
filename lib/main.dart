d64a5d1eaf5590aa0af3c6b7f01bdfa0755764ff
Paulo <pauloh_gl@hotmail.com>
2023-07-26 14:58:53 -0300

    Classe HomePage
    Extends StatelessWidget, função increment() e interpolação de string com  variável.

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,36 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(primarySwatch: Colors.purple),
-    home: Scaffold(
+    home: HomePage()
+  ));
+}
+
+class HomePage extends StatelessWidget { // Um widget que não requer estado mutável.
+  var count = 5;
+  void increment() {
+    count++;
+    print(count);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Meu Primeiro App"),
       ),
-      body: const Center(
+      body: Center(
         child: Text(
-          'Contador\n0',
+          "Contador\n$count", // "Contador\n" + count.toString()
           textAlign: TextAlign.center,
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () => {}
+        onPressed: () => {
+          increment()
+        }
       ),
-    )
-  ));
+    );
+  }
+
 }
