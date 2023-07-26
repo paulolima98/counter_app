363223f9bc7d24171b068d0d64194a883f0d2769
Paulo <pauloh_gl@hotmail.com>
2023-07-26 12:07:34 -0300

    Primeiros Conceitos
    MaterialApp, Container, ThemeData, Scaffold, AppBar e Text.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    color: Colors.purple
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Primeiro App"),
+      ),
+      body: Container(
+        // width: 500,
+        // height: 500,
+        color: Colors.purple,
+        child: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        )
+      ),
+    )
   ));
 }
